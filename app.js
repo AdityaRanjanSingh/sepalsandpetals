9339458322acf3b89422ba5eeaaed9f36412bd31
AdityaRanjanSingh <adityaranjan321@gmail.com>
2017-08-25 17:24:27 +0530

    changes in the type function

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 
 function type(d){
-
+	d.sepal_length = +d.sepal_length;
+	d.petal_length = +d.petal_length;
+	d.sepal_width = +d.sepal_width;
+	d.petal_width = +d.petal_width;
 }
 d3.csv("iris.csv",type,function (array) {
 	// body...
